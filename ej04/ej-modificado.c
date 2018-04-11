aeded46a14305443292922e383b0e15a1efc9c1e
danibarria <barriareyesjosedaniel@gmail.com>
2018-04-11 07:52:19 -1100

    ejercicio 4 terminado

---
+++ ej04/ej-modificado.c
@@ -0,0 +1,28 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+#define TRUE 1
+
+int main() {
+    int status;
+    char *args[2];
+
+    args[0] = "/bin/ls";
+    // args[1] = "-l";
+    args[1] = "-ltr";
+    args[2] = NULL;
+
+    if (fork() != 0) /* Código del padre */
+    {
+        printf("\nPADRE\n");
+        waitpid(-1, &status,0);
+    }else{
+        printf("\n%d\n", execv( args[0], args));
+    }/* Código del hijo */  
+    
+    return 0;
+}
---
+++ ej04/explicacion.md
@@ -0,0 +1,4 @@
+A.  escribe por consola PADRE e inicia un proceso de firefox.
+El padre espera que termine el proceso hijo
+
+B.  
